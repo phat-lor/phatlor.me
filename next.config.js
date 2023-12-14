7923cebed0627ddff0b74d69cd819fd2141a35af
Pat <153535555+phat-lor@users.noreply.github.com>
2023-12-14 17:50:43 +0700

    remove obfuscation

--- next.config.js
+++ next.config.js
@@ -1,24 +1,23 @@
 const NextJSObfuscatorPlugin = require("nextjs-obfuscator");
 
 const nextConfig = {
-  webpack: (config, { dev }) => {
-    if (!dev) {
-      config.plugins.push(
-        new NextJSObfuscatorPlugin(
-          {
-            ...nextjsoptions,
-          },
-          {
-            obfuscateFiles: {
-              app: true,
-              pages: true,
-            },
-            log: true,
-          }
-        )
-      );
-    }
-
-    return config;
-  },
+  // webpack: (config, { dev }) => {
+  //   if (!dev) {
+  //     config.plugins.push(
+  //       new NextJSObfuscatorPlugin(
+  //         {
+  //           ...nextjsoptions,
+  //         },
+  //         {
+  //           obfuscateFiles: {
+  //             app: true,
+  //             pages: true,
+  //           },
+  //           log: true,
+  //         }
+  //       )
+  //     );
+  //   }
+  //   return config;
+  // },
 };
